2fb27e1eb0eb030f94f1691692e18a83d58c6ae3
fardaaannn <fardaaannn@gmail.com>
2026-01-03 17:30:23 +0700

    Update CACHE_NAME to version 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dompetku-v1";
+const CACHE_NAME = "dompetku-v2";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
@@ -43,3 +43,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
